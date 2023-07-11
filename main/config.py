caa6a29783dbe0a728cbc9da055c587de887fbb7
Ayan Nandi <110126109+ayanNandi410@users.noreply.github.com>
2023-07-11 19:49:54 +0530

    Update config.py

--- main/config.py
+++ main/config.py
@@ -1,6 +1,5 @@
  import os
 import secrets
-import logging
 
 baseDir = os.path.abspath(os.getcwd())
 
@@ -16,9 +15,4 @@ class LocalDevConfig():
     #"sqlite:///" + os.path.join(SQLITE_DB_DIR, "ticketdb.sqlite")
     SECRET_KEY = secrets.token_hex(26)
     DEBUG=True
-
-    if app.config['LOG_WITH_GUNICORN']:
-        gunicorn_error_logger = logging.getLogger('gunicorn.error')
-        app.logger.handlers.extend(gunicorn_error_logger.handlers)
-        app.logger.setLevel(logging.DEBUG)
 
